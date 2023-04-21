e72daa07316c9386e93dac2938ba0011bcc2e9cf
Abhishek Singh <abhishek2511.25@gmail.com>
2023-04-21 19:07:09 +0530

    Successfully connected to the database

---
+++ config/mongoose.js
@@ -0,0 +1,15 @@
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1/codeial_development');
+
+const db = mongoose.connection;
+
+db.on('error',console.error.bind(console, "Error Connecting to MongoDB...."));
+
+db.once('open', function(){
+    console.log('Connected to  DataBase :: MongoDB');
+});
+
+//to make this file usable we need to export it
+module.exports = db;
